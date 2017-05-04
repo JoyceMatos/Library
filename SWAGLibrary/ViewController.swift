a98d84784f8b6b47869b06f54ae4e73f234898b2
JoyceMatos <j.matosjoyce@gmail.com>
2017-05-04 11:34:41 -0400

    initial commit

---
+++ SWAGLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SWAGLibrary
+//
+//  Created by Joyce Matos on 5/4/17.
+//  Copyright © 2017 Joyce Matos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
